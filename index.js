7b2b8ea5e5a073dfcda95c420ab0d7277398994e
Tarakanoed <svitch1337@gmail.com>
2024-04-16 18:14:40 +0500

    3-4

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let count = 1;
+const forms = document.querySelectorAll(".beverage");
+const form = forms[forms.length - 1].cloneNode(true);
+
 document.querySelector(".add-button").addEventListener("click", () => {
     count++;
-    let forms = document.querySelectorAll(".beverage");
-    let form = forms[forms.length - 1].cloneNode(true);
+
     form.querySelector('input[type="radio"][value="usual"]').name = `milk${count}`;
     form.querySelector("h4").innerHTML = `Напиток №${count}`;
     form.querySelector('select').selectedIndex = 1;
@@ -13,4 +15,25 @@ document.querySelector(".add-button").addEventListener("click", () => {
 
 
     forms[forms.length - 1].after(form);
+});
+
+const submitButton = document.querySelector('.submit-button');
+const modalOverlay = document.createElement('div');
+
+
+document.querySelector('.submit-button').addEventListener('click', (event) => {
+    event.preventDefault();
+    const modalContent = `<div class="modal-content">
+                            <p>Заказ принят!</p>
+                            <span class="close-button">X</span>
+                          </div>`;
+
+    modalOverlay.classList.add('modal-overlay');
+    modalOverlay.innerHTML = modalContent;
+    document.body.appendChild(modalOverlay);
+
+    const closeButton = modalOverlay.querySelector('.close-button');
+    closeButton.addEventListener('click', function() {
+        modalOverlay.remove();
+    });
 });
